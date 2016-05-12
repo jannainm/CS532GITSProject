2bd8f2bd732b999b04a58a4be2017df9de181823
Michael Jannain <jannainm@gmail.com>
2016-05-12 15:21:30 -0700

    Deleted old files - Deliverables #4 in project files folder.

--- GITSDatabase/OnlineUI/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jannainm/GitHub/CS532GITSProject/GITSDatabase/OnlineUI/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
